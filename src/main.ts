c0ea528541d6ba5bb786165540ed6eda0132ed07
Simen A. W. Olsen <so@bjerk.io>
2021-04-14 09:26:05 +0200

    feat: Add deprecation notice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { setFailed, group } from '@actions/core';
+import { setFailed, group, warning } from '@actions/core';
 import getVaultSecret from './getVaultSecret';
 
 export const run = async (): Promise<void> => {
   try {
+    warning(
+      'This Github Action is deprecated. Read more about it here: https://github.com/bjerkio/vault-action/issues/393',
+    );
     await group('Get Vault Secrets', getVaultSecret);
   } catch (error) {
     const message = error.message || 'Failed';
